ae50d42dbc6045442984fe6c2f06b452bd7e539b
Sumeyye Cigdem <s.gulsum.cigdem@hotmail.com>
2023-03-28 13:52:22 +0300

    Cydeo-cucumber-junit

---
+++ src/test/java/com/cydeo/step_definitions/WebTable_StepDefinitions.java
@@ -0,0 +1,35 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class WebTable_StepDefinitions {
+
+    @Given("user is on the login page of web table app")
+    public void user_is_on_the_login_page_of_web_table_app() {
+
+    }
+
+    @When("user enters  username {string}")
+    public void user_enters_username(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters password {string}")
+    public void user_enters_password(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks to login button")
+    public void user_clicks_to_login_button() {
+
+    }
+    @Then("user should see url contains order")
+    public void user_should_see_url_contains_order() {
+
+    }
+
+
+}
--- target/test-classes/features/dataTable.feature
+++ target/test-classes/features/dataTable.feature
@@ -1,6 +1,5 @@
 Feature: Examples of Cucumber data table implementations
 
-  @wip
   Scenario: List of fruits I like
     Then user should see fruits I like
       | kiwi        |
